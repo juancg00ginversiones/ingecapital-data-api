b480f78a352e98bd3e4ededceab2b063d3a66a9b
juancg00ginversiones <juancg00ginversiones@gmail.com>
2025-12-30 18:13:48 -0300

    Create tickers.py

---
+++ tickers.py
@@ -0,0 +1,22 @@
+TICKERS_BY_SECTOR = {
+    "big_tech": [
+        "AAPL","MSFT","GOOGL","AMZN","META","NVDA","TSLA","NFLX","ADBE",
+        "ORCL","CRM","INTC","AMD","QCOM","AVGO","IBM",
+        "SNOW","NOW","UBER","SHOP","PANW"
+    ],
+    "financials": [
+        "JPM","BAC","WFC","GS","MS","V","MA","BRK-B"
+    ],
+    "consumer_industrial": [
+        "WMT","KO","PEP","MCD","DIS","NKE","HD","COST","PG"
+    ],
+    "energy_commodities": [
+        "XOM","CVX","SLB"
+    ],
+    "crypto_etf": [
+        "IBIT","ETHA"
+    ],
+    "argentina_adr": [
+        "YPF","GGAL","PAM","BMA","CEPU","EDN","SUPV","TGS","TEO"
+    ]
+}
