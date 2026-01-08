23ae8513a4804eb4e6f029a0aad5842591f949bd
juancg00ginversiones <juancg00ginversiones@gmail.com>
2026-01-08 11:33:24 -0300

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -31,12 +31,24 @@ def _get_lock_and_event(ticker: str):
 # ============================================================
 def rsi(series: pd.Series, period: int = 14) -> pd.Series:
     delta = series.diff()
-    gain = (delta.where(delta > 0, 0))
-    loss = (-delta.where(delta < 0, 0))
-    avg_gain = gain.ewm(alpha=1/period, adjust=False).mean()
-    avg_loss = loss.ewm(alpha=1/period, adjust=False).mean()
+    gain = delta.where(delta > 0, 0)
+    loss = -delta.where(delta < 0, 0)
+
+    avg_gain = gain.ewm(
+        alpha=1/period,
+        min_periods=period,
+        adjust=False
+    ).mean()
+
+    avg_loss = loss.ewm(
+        alpha=1/period,
+        min_periods=period,
+        adjust=False
+    ).mean()
+
     rs = avg_gain / avg_loss
     return 100 - (100 / (1 + rs))
+
 
 
 def macd_bias(series: pd.Series) -> str:
